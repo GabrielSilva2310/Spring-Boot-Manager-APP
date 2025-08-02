cec91eb7040be9a55200b356842855d0c10ffe81
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2025-08-02 17:52:20 -0300

    Created Project

---
+++ src/test/java/com/mybusiness/managerapp/ManagerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybusiness.managerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
